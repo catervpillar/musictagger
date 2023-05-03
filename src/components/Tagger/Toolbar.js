cd52dbf78f81c7d6c1690a4af8f49ed2600c7239
Tommaso Catervi <56674392+catervpillar@users.noreply.github.com>
2023-05-03 03:06:38 +0000

    update toolbar

--- src/components/Tagger/Toolbar.js
+++ src/components/Tagger/Toolbar.js
@@ -1,24 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRefresh, faEraser, faUpload } from '@fortawesome/free-solid-svg-icons'
+import { faRefresh, faEraser, faUpload, faDownload, faFileAudio } from '@fortawesome/free-solid-svg-icons'
 
 import classes from './Toolbar.module.css';
 
 const Toolbar = () => {
     return (
         <nav className={classes.toolbar}>
-            <button>
-                <FontAwesomeIcon icon={faRefresh} />
-                Reset Tags
-            </button>
-            <button>
-                <FontAwesomeIcon icon={faEraser} />
-                Clear All Tags
-            </button>
-            <button>
-                <FontAwesomeIcon icon={faUpload} />
-                Upload New File
-            </button>
-            <button>Download My Song!</button>
+            <div className={classes['current-file']}>
+                <p>NOW EDITING</p>
+                <div className={classes.name}>
+                    <FontAwesomeIcon icon={faFileAudio} />
+                    <h3>T-Square - Truth.mp3</h3>
+                </div>
+            </div>
+            <div className={classes.actions}>
+                <button>
+                    <FontAwesomeIcon icon={faRefresh} />
+                    Reset Tags
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faEraser} />
+                    Clear All Tags
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faUpload} />
+                    Upload New File
+                </button>
+                <button className={classes['download-button']}>
+                    <FontAwesomeIcon icon={faDownload} />
+                    Download My Song!
+                </button>
+            </div>
         </nav>
     );
 };
